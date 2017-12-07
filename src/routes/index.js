99c7c7a9261d8fa16835917fbbc2b3ccce748c91
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-12-07 01:49:01 -0200

    Atualizando para nova versão do botkit-fb-bootstrap

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
-const incomingWebhooks = require('./incoming_webhooks.js');
 const envConfig = require('./env_config.js');
 
-const routes = [envConfig, incomingWebhooks];
+const routes = [envConfig];
 
 module.exports = routes;
